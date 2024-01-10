2898afd7f5a00cfcc095af65d85b8b6b1688beaa
Dav-Git <mail@david-bauch.de>
2024-01-10 12:30:33 +0100

    commit build

---
+++ lib/types/results.d.ts
@@ -0,0 +1,14 @@
+interface Result {
+    success: boolean;
+}
+interface DeparturesResult extends Result {
+    departures?: FPTFStopover[];
+    situations?: Situation[];
+}
+interface JourneysResult extends Result {
+    journeys?: FPTFJourney[];
+    situations?: Situation[];
+}
+interface StopsResult extends Result {
+    stops?: FPTFStop[];
+}
